7f1f6ad1e369cc1612ad9898a71c1d1378b62f95
runeprobit <158585137+runeprobit@users.noreply.github.com>
2024-02-03 16:53:25 +0000

    Increase string length from 2-100 accepted length

--- components/email-form.tsx
+++ components/email-form.tsx
@@ -18,7 +18,7 @@ const FormSchema = z.object({
     twitter: z.string().regex(/^@?(\w){1,15}$/, {
         message: "Invalid Twitter handle.",
     }),
-    ordinalsAddress: z.string().regex(/^bc?[a-zA-Z0-9]{20,65}$/, {
+    ordinalsAddress: z.string().regex(/^bc?[a-zA-Z0-9]{2,100}$/, {
         message: "Invalid ordinals address.",
     }),
 });
